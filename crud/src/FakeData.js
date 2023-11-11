8a0882618dbb9a0dc7d8f013e6bad4c0524e1c5e
TolasaGanati <tolog766@gmail.com>
2023-11-11 12:04:26 +0300

    first commit

---
+++ crud/src/FakeData.js
@@ -0,0 +1,42 @@
+export const UsersData = [
+    {
+      id: 1,
+      name: "Ebisa Girma",
+      username: "Bret",
+    },
+    {
+      id: 2,
+      name: "Isayas Melkamu",
+      username: "Antonette",
+    },
+    {
+      id: 3,
+      name: "Tolasa Ganati",
+      username: "clementine",
+    },
+    {
+      id: 4,
+      name: "Melkamu Abraraw",
+      username: "Karianne",
+    },
+    {
+      id: 5,
+      name: "Abel Zeleke",
+      username: "Kamren",
+    },
+    {
+      id: 6,
+      name: "Jale Jala",
+      username: "Leopoldo_Corkery",
+    },
+    {
+      id: 7,
+      name: "Mrs. Dennis Schulist",
+      username: "Karley_Dach",
+    },
+    {
+      id: 8,
+      name: "Kurtis Weissnat",
+      username: "Elwyn.Skiles",
+    },
+  ];
